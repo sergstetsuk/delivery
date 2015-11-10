eddc5537f45b7cc523d8b269dc5a32c2f8035a4e
Serg Stetsuk <serg_stetsuk@ukr.net>
2015-11-10 22:35:43 +0200

    Added search dialog close button. Filtering works in Clients. Check icon in Orders instead of Add icon. Todo: normal keyboard show/hide operation on SearchDialog.

--- app/src/main/java/ua/com/vodapitna/delivery/ClientsAdapter.java
+++ app/src/main/java/ua/com/vodapitna/delivery/ClientsAdapter.java
@@ -18,90 +18,101 @@ import android.util.Log;
 
 public class ClientsAdapter extends BaseAdapter {
 
-    private static LayoutInflater inflater = null;
-    private FragmentActivity mActivity;
-    private SQLHandler mDbHandler;
-    private Cursor mCursor;
+	private static LayoutInflater inflater = null;
+	private FragmentActivity mActivity;
+	private SQLHandler mDbHandler;
+	private Cursor mCursor;
+	private String mFilter = "";
 
-    public ClientsAdapter(FragmentActivity a) {
-        mActivity = a;
-        mDbHandler = new SQLHandler(a.getBaseContext());
-        notifyDataSetChanged();
-        inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+	public ClientsAdapter(FragmentActivity a) {
+		mActivity = a;
+		mDbHandler = new SQLHandler(a.getBaseContext());
+		notifyDataSetChanged();
+		inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
 
-    @Override
-    public void notifyDataSetChanged() {
-        Spinner sortselect = (Spinner) mActivity.findViewById(R.id.svClientsSortMode);
-        if(sortselect==null || sortselect.getSelectedItemId()==0) {
-            mCursor = mDbHandler.selectQuery("SELECT c.* FROM clients c ORDER BY c.addr ASC;");
-        } else {
-            mCursor = mDbHandler.selectQuery("SELECT c.* FROM clients c ORDER BY c.name ASC;");
-        }
-        super.notifyDataSetChanged();
-    }
+	@Override
+	public void notifyDataSetChanged() {
+		Spinner sortselect = (Spinner) mActivity.findViewById(R.id.svClientsSortMode);
+		String SQLWhere = "";
+		if(sortselect==null || sortselect.getSelectedItemId()==0) {
+			mCursor = mDbHandler.selectQuery("SELECT c.* FROM clients c " + mFilter + " ORDER BY c.addr ASC;");
+		} else {
+			mCursor = mDbHandler.selectQuery("SELECT c.* FROM clients c " + mFilter + " ORDER BY c.name ASC;");
+		}
+		super.notifyDataSetChanged();
+	}
 
-    public int getCount() {
-        return mCursor.getCount();
-    }
+	public void setFilter(String s){
+		if (s.length() > 0) {
+			mFilter = "WHERE c.name LIKE '" + s + "%' OR c.addr LIKE '" + s + "%'";
+		} else {
+			mFilter = "";
+		}
+	}
 
-    public Object getItem(int position) {
-        return position;
-    }
+	public int getCount() {
+		return mCursor.getCount();
+	}
 
-    public long getItemId(int position)
-    {
-        mCursor.moveToPosition(position);
-        long index = mCursor.getLong(mCursor.getColumnIndex("id"));
-        return index;
-    }
+	public Object getItem(int position) {
+		return position;
+	}
 
-    public View getView(int position, View convertView, ViewGroup parent) {
-        mCursor.moveToPosition(position);
-        View vi = convertView;
-        if (convertView == null)
-            vi = inflater.inflate(R.layout.clients_adapter, null);
+	public long getItemId(int position)
+	{
+		mCursor.moveToPosition(position);
+		long index = mCursor.getLong(mCursor.getColumnIndex("id"));
+		return index;
+	}
 
-        TextView name = (TextView) vi.findViewById(R.id.name); // name
-        final TextView address = (TextView) vi.findViewById(R.id.address);
-        final TextView telephone = (TextView) vi.findViewById(R.id.telephone);
-	final long id = getItemId(position);
-        ImageButton btCall = (ImageButton) vi.findViewById(R.id.callButton);
+	public View getView(int position, View convertView, ViewGroup parent) {
+		mCursor.moveToPosition(position);
+		View vi = convertView;
+		if (convertView == null)
+			vi = inflater.inflate(R.layout.clients_adapter, null);
 
-        //click listener for CALL action
-        final View.OnClickListener makeCallListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telephone.getText()));
-                inflater.getContext().startActivity(intent);
-            }
-        };
+		TextView name = (TextView) vi.findViewById(R.id.name); // name
+		final TextView address = (TextView) vi.findViewById(R.id.address);
+		final TextView telephone = (TextView) vi.findViewById(R.id.telephone);
+		final long id = getItemId(position);
+		ImageButton btCall = (ImageButton) vi.findViewById(R.id.callButton);
 
-        btCall.setOnClickListener(makeCallListener);
+		//click listener for CALL action
+		final View.OnClickListener makeCallListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//todo: select phone dialog
+				Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telephone.getText()));
+				inflater.getContext().startActivity(intent);
+			}
+		};
 
-        ImageButton btAddOrder = (ImageButton) vi.findViewById(R.id.addOrderButton);
+		btCall.setOnClickListener(makeCallListener);
 
-        //click listener for CALL action
-        final View.OnClickListener addOrderListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OrdersEditFragment orderseditdialog = new OrdersEditFragment();
-                Bundle b = new Bundle();
-                b.putString("clientid", String.valueOf(id));
-                orderseditdialog.setArguments(b);
-                orderseditdialog.show(mActivity.getSupportFragmentManager(), null);
-            }
-        };
+		ImageButton btAddOrder = (ImageButton) vi.findViewById(R.id.addOrderButton);
 
-        btAddOrder.setOnClickListener(addOrderListener);
+		//click listener for CALL action
+		final View.OnClickListener addOrderListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				OrdersEditFragment orderseditdialog = new OrdersEditFragment();
+				Bundle b = new Bundle();
+				b.putString("clientid", String.valueOf(id));
+				orderseditdialog.setArguments(b);
+				orderseditdialog.show(mActivity.getSupportFragmentManager(), null);
+			}
+		};
 
-        name.setText(mCursor.getString(mCursor.getColumnIndex("name")));
-        address.setText(mCursor.getString(mCursor.getColumnIndex("cat"))+
-                " "+mCursor.getString(mCursor.getColumnIndex("addr"))+
-                ", "+mCursor.getString(mCursor.getColumnIndex("house"))+
-                "/"+mCursor.getString(mCursor.getColumnIndex("office")));
-        telephone.setText(mCursor.getString(mCursor.getColumnIndex("phone")));
-	/*todo: need to check phone1 and show*/
-        return vi;
-    }
+		btAddOrder.setOnClickListener(addOrderListener);
+
+		name.setText(mCursor.getString(mCursor.getColumnIndex("name")));
+		address.setText(mCursor.getString(mCursor.getColumnIndex("cat"))
+			+ " " + mCursor.getString(mCursor.getColumnIndex("addr"))
+			+ ", " + mCursor.getString(mCursor.getColumnIndex("house"))
+			+ "/" + mCursor.getString(mCursor.getColumnIndex("office")));
+		telephone.setText(mCursor.getString(mCursor.getColumnIndex("phone")));
+		/*todo: need to check phone1 and show*/
+		return vi;
+	}
 }
--- app/src/main/java/ua/com/vodapitna/delivery/ClientsFragment.java
+++ app/src/main/java/ua/com/vodapitna/delivery/ClientsFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Spinner;
@@ -14,6 +15,8 @@ import android.widget.EditText;
 import android.view.WindowManager;
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
+import android.text.TextWatcher;
+import android.text.Editable;
 
 public class ClientsFragment extends Fragment {
 
@@ -32,13 +35,39 @@ public class ClientsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.clients_fragment, container, false);
 
+	final View searchdialog = (View) v.findViewById(R.id.ClientsSearchDialog);
         TextView tv = (TextView) v.findViewById(R.id.tvHeader);
         final ListView lv = (ListView) v.findViewById(R.id.lvClientsList);
         Spinner spinOrder = (Spinner) v.findViewById(R.id.svClientsSortMode);
         Button btadd = (Button) v.findViewById(R.id.btAddContactButton);
         Button btsearch = (Button) v.findViewById(R.id.btSearchContactButton);
+        ImageButton btclosesearch = (ImageButton) v.findViewById(R.id.closeSearch);
 	final EditText searchtext = (EditText) v.findViewById(R.id.ClientsSearchText);
         ClientsAdapter adapter = new ClientsAdapter(getActivity());
+	searchtext.addTextChangedListener(new TextWatcher(){
+		@Override
+		public void afterTextChanged(Editable s){
+			ClientsAdapter ad = (ClientsAdapter) lv.getAdapter();
+			ad.setFilter(s.toString());
+			ad.notifyDataSetChanged();
+		}
+		@Override
+		public void beforeTextChanged(CharSequence s,int start,int count,int before){
+		}
+		@Override
+		public void onTextChanged(CharSequence s,int start,int count,int before){
+		}
+	});
+        btclosesearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+		searchtext.setText("");
+		searchdialog.setVisibility(View.GONE);
+                ClientsAdapter ad = (ClientsAdapter) lv.getAdapter();
+                ad.notifyDataSetChanged();
+
+            }
+        });
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -49,6 +78,16 @@ public class ClientsFragment extends Fragment {
                 clienteditdialog.show(getFragmentManager(), null);
             }
         });
+        /*lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public void onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                ClientsEditFragment clienteditdialog = new ClientsEditFragment();
+                Bundle b = new Bundle();
+                b.putString("clientid", String.valueOf(id));
+                clienteditdialog.setArguments(b);
+                clienteditdialog.show(getFragmentManager(), null);
+            }
+        });*/
         spinOrder.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -83,11 +122,11 @@ public class ClientsFragment extends Fragment {
             public void onClick(View v) {
                 //~ SQLHandler mDbHandler = new SQLHandler(getContext());
                 //~ mDbHandler.executeQuery("DELETE FROM clients;");
-		searchtext.setVisibility(View.VISIBLE);
+		searchdialog.setVisibility(View.VISIBLE);
 		searchtext.requestFocus();
 		//getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-		InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-		    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+		//~ InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+		    //~ imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
                 /*ClientsEditFragment clienteditdialog = new ClientsEditFragment();
                 Bundle b = new Bundle();
                 b.putString("clientid","2");
@@ -95,7 +134,6 @@ public class ClientsFragment extends Fragment {
                 clienteditdialog.show(getFragmentManager(),null);*/
                 ClientsAdapter ad = (ClientsAdapter) lv.getAdapter();
                 ad.notifyDataSetChanged();
-
             }
         });
 
