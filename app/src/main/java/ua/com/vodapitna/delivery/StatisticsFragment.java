50da90f887578adbe1143a5738d0ddbd0cc76039
Serg Stetsuk <serg_stetsuk@ukr.net>
2015-12-15 06:47:29 +0200

    Activated statistics view. Need proper filling

--- app/src/main/java/ua/com/vodapitna/delivery/StatisticsFragment.java
+++ app/src/main/java/ua/com/vodapitna/delivery/StatisticsFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.text.Html;
 
 public class StatisticsFragment extends Fragment {
 
@@ -24,9 +25,20 @@ public class StatisticsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.statistics_fragment, container, false);
 
-        TextView tv = (TextView) v.findViewById(R.id.tvFragFourth);
-        tv.setText(getArguments().getString("msg"));
-
+        TextView tv = (TextView) v.findViewById(R.id.tvStatistics);
+        tv.setText(Html.fromHtml("<b>Кількість:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість1:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість2:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість3:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість4:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість5:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість6:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість7:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість8:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість9:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість10:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість11:</b> %a<br/><b>Кілометраж:</b> %b<br/>"
+	    +"<b>Кількість12:</b> %a<br/><b>Кілометраж:</b> %b<br/>"));
         return v;
     }
 }
